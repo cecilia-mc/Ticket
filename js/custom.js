54886e6e1c27db30920b2eaba73f47fe135472ce
Cecilia <ceciliam3592@gmail.com>
2017-02-21 10:47:53 -0500

    Practica

---
+++ js/custom.js
@@ -0,0 +1,96 @@
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 64)
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 65
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// HTML5 Placeholder
+$(function() {
+    $('input, textarea').placeholder();
+});
+
+// Load WOW.js on non-touch devices
+var isPhoneDevice = "ontouchstart" in document.documentElement;
+$(document).ready(function() {
+    if (isPhoneDevice) {
+        //mobile
+    } else {
+        //desktop
+        // Initialize WOW.js
+        wow = new WOW({
+            offset: 50
+        })
+        wow.init();
+    }
+});
+
+/* Fancy restrictive input formatting via jQuery.payment library*/
+$('input[name=cardNumber]').payment('formatCardNumber');
+$('input[name=cardCVC]').payment('formatCardCVC');
+$('input[name=cardExpiry').payment('formatCardExpiry');
+
+/* Form validation using Stripe client-side validation helpers */
+jQuery.validator.addMethod("cardNumber", function(value, element) {
+    return this.optional(element) || Stripe.card.validateCardNumber(value);
+}, "Please specify a valid credit card number.");
+
+jQuery.validator.addMethod("cardExpiry", function(value, element) {    
+    /* Parsing month/year uses jQuery.payment library */
+    value = $.payment.cardExpiryVal(value);
+    return this.optional(element) || Stripe.card.validateExpiry(value.month, value.year);
+}, "Invalid expiration date.");
+
+jQuery.validator.addMethod("cardCVC", function(value, element) {
+    return this.optional(element) || Stripe.card.validateCVC(value);
+}, "Invalid CVC.");
+
+validator = $form.validate({
+    rules: {
+        cardNumber: {
+            required: true,
+            cardNumber: true            
+        },
+        cardExpiry: {
+            required: true,
+            cardExpiry: true
+        },
+        cardCVC: {
+            required: true,
+            cardCVC: true
+        }
+    },
+    highlight: function(element) {
+        $(element).closest('.form-control').removeClass('success').addClass('error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-control').removeClass('error').addClass('success');
+    },
+    errorPlacement: function(error, element) {
+        $(element).closest('.form-group').append(error);
+    }
+});
